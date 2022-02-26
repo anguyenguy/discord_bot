ab59f6d79a05f91dbd349695901b138ccf81a57e
unknown <hdragon1501@gmail.com>
2022-02-26 19:43:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+function getQuote(){
+    return fetch("http://zenquotes.io/api/random")
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        return data[0]["q"]+"-"+data[0]["a"]
+    })
+}
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+})
+
+client.on("message", msg => {
+    if(msg.content === "ping"){
+        msg.reply(`Hello ${client.user.tag}`);
+    }
+
+    if(msg.content === "hi"){
+        getQuote().then(quote => msg.channel.send(quote))
+    }
+})
+
+client.login(config.BOT_TOKEN);
